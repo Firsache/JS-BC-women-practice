9c0b71074e88eecca69249835c91ed67b926e107
Firsova Liubov <firsova.lyub@gmail.com>
2022-12-16 17:56:40 +0200

    task 1

---
+++ script.js
@@ -0,0 +1,26 @@
+const pizzaPalace = {
+  pizzas: ["Ultracheese", "Smoked", "Four meats"],
+    order(pizzaName, onSuccess, onError){
+        if(!this.pizzas.includes(pizzaName)){
+        return onError(`There is no pizza with a name ${pizzaName} in the assortment.`)
+        }
+        return onSuccess(pizzaName);
+    },
+};
+// Change code above this line
+
+// Callback for onSuccess
+function makePizza(pizzaName) {
+  return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+}
+
+// Callback for onError
+function onOrderError(error) {
+  return `Error! ${error}`;
+}
+
+// Method calls with callbacks
+console.log(pizzaPalace.order("Smoked", makePizza, onOrderError));
+console.log(pizzaPalace.order("Four meats", makePizza, onOrderError));
+console.log(pizzaPalace.order("Big Mike", makePizza, onOrderError));
+console.log(pizzaPalace.order("Vienna", makePizza, onOrderError));
